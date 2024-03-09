fead513fd7e91056e491687d4928a260d6c52014
ChethanKodenkiri <chethankodenkiri@gmail.com>
2024-03-09 12:57:38 +0530

    update : added components to frontend <signed by : Chethan Koenkiri>

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,33 @@
 import './App.css'
+import Header from './components/Header';
+import { Routes,Route } from 'react-router-dom';
+import {Home} from './pages/Home'
+import Chat from './pages/Chat';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import NotFound from './pages/NotFound';
+
 
 function App() {
 
   return (
- <main><h1>Hello world</h1></main>
+ <main>
+  <Header/>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/chat' element={<Chat />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/notfound' element={<NotFound />} />
+    </Routes>
+
+
+
+ </main>
   )
 }
 
 export default App
+
+
+//Time 2:10:30
