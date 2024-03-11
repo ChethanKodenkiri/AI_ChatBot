5a10daaaa3b7893f915790643458b23dbf650534
ChethanKodenkiri <chethankodenkiri@gmail.com>
2024-03-11 23:20:27 +0530

    update added provide context <signed by: chethan kodenkiri

--- Frontend/src/components/conext/AuthContext.tsx
+++ Frontend/src/components/conext/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useEffect, useState } from "react";
 
 
 type User={
@@ -15,5 +15,22 @@ type UserAuth ={
 
 const AuthContext =  createContext<UserAuth|null>(null);
 const AuthProvider = ({children}:{children:ReactNode})=>{
+    const [user,setUser] = useState<User|null>(null);
+    const [isLoggedIn,setIsLoggedIn] =useState(false);
 
+    useEffect(()=>{
+
+    },[]);
+
+    const login = async(email:String,password:String)=>{};
+    const signUp = async(name:String,email:String,password:String)=>{};
+    const logout = async()=>{}
+
+    const value ={
+        user,
+        isLoggedIn,
+        login,
+        signUp,
+        logout
+    }
 }
